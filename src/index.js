db0500c75f70a094eaf375b890a03e8d4173bdb7
Dinis <d.yudenka@yandex.by>
2022-01-28 05:45:10 +0300

    feat: add a solution to  the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if(!str.length) return false;
+
+  let arr = [],
+      resultStr = str,
+      resultStatus;
+      
+  bracketsConfig.forEach(item => arr.push(item[0] + item[1]));
+
+  for (;;) {
+    let statusBrackets = '';
+
+    arr.forEach(item => {
+        if (resultStr.includes(item)) {
+          statusBrackets += '1';
+          resultStr = resultStr.replaceAll(item, '')
+      } else {
+          statusBrackets += '';
+      }      
+    }) 
+
+    if (!resultStr.length) {
+        resultStatus = true;
+        break;
+    } else if (!statusBrackets) {
+        resultStatus = false;
+        break;
+    }
+  }
+  return resultStatus
 }
